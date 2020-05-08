549f2e27c3db79880e84e2d31f96cdd0df2fa08f
Marcos Corsi <marcosvcorsi@gmail.com>
2020-05-07 23:21:07 -0300

    Aplicando TDD

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 import authConfig from '@config/auth';
@@ -7,6 +6,7 @@ import AppError from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   email: string;
@@ -22,12 +22,18 @@ class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
 
-    if (!user || !(await compare(password, user.password))) {
+    if (
+      !user ||
+      !(await this.hashProvider.compareHash(password, user.password))
+    ) {
       throw new AppError('Incorret email/password combination.', 401);
     }
 
