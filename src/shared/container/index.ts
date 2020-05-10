0b884553bdbae59750f8e706a79eb18d79be24dd
Marcos Corsi <marcosvcorsi@gmail.com>
2020-05-10 20:52:10 -0300

    Forgot password adicionado

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,12 +6,20 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 import '@modules/users/providers';
 import './providers';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
 
 container.registerSingleton<IAppointmentsRepository>(
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProdiver from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerSingleton<IMailProvider>(
+  'MailProvider',
+  EtherealMailProdiver,
 );
